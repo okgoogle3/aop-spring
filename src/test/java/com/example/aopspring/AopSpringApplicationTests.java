16b3380ab8ab9139b2e5da5e4776ee9201152b18
okgoogle3 <halalah.han@gmail.com>
2022-07-12 14:43:29 +0300

    Test

--- src/test/java/com/example/aopspring/AopSpringApplicationTests.java
+++ src/test/java/com/example/aopspring/AopSpringApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.aopspring;
 
+import com.example.aopspring.service.ServiceTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AopSpringApplicationTests {
-
+    @Autowired
+    ServiceTest test;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testAnnotation() {
+        String result = test.print("test");
+        assertEquals("zalupatest", result);
     }
 
 }
